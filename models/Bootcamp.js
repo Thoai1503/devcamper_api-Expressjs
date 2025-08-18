bd112370ea8c4a9ceaf32992491e9362db47bda3
Thoai1503 <vothoai1503@gmail.com>
2025-08-18 11:12:19 +0700

    first commit

---
+++ models/Bootcamp.js
@@ -0,0 +1,183 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
+
+const BootcampSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Name can not be more than 50 characters"],
+    },
+    slug: String,
+    description: {
+      type: String,
+      required: [true, "Please add a description"],
+      maxlength: [500, "Description can not be more than 500 characters"],
+    },
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        "Please use a valid URL with HTTP or HTTPS",
+      ],
+    },
+    phone: {
+      type: String,
+      maxlength: [20, "Phone number can not be longer than 20 characters"],
+    },
+    email: {
+      type: String,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    address: {
+      type: String,
+      required: [true, "Please add an address"],
+    },
+    location: {
+      // GeoJSON Point
+      type: {
+        type: String,
+        enum: ["Point"],
+      },
+      coordinates: {
+        type: [Number],
+        index: "2dsphere",
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String,
+    },
+    careers: {
+      // Array of strings
+      type: [String],
+      required: true,
+      enum: [
+        "Web Development",
+        "Mobile Development",
+        "UI/UX",
+        "Data Science",
+        "Business",
+        "Other",
+      ],
+    },
+    averageRating: {
+      type: Number,
+      min: [1, "Rating must be at least 1"],
+      max: [10, "Rating must can not be more than 10"],
+    },
+    averageCost: Number,
+    photo: {
+      type: String,
+      default: "no-photo.jpg",
+    },
+    housing: {
+      type: Boolean,
+      default: false,
+    },
+    jobAssistance: {
+      type: Boolean,
+      default: false,
+    },
+    jobGuarantee: {
+      type: Boolean,
+      default: false,
+    },
+    acceptGi: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    meow: { virtuals: true },
+  }
+);
+
+BootcampSchema.statics.getBootcampById = async function (id) {
+  const bootcamp = await this.findById(id);
+  if (!bootcamp) {
+    throw new Error(`Bootcamp not found with id of ${id}`);
+  }
+  return bootcamp;
+};
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre("save", async function (next) {
+  //  BootcampSchema.toJSON();
+
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+  next();
+});
+
+// Cascade delete courses when a bootcamp is deleted
+// BootcampSchema.pre("remove", async function (next) {
+//   console.log(`Courses being removed from bootcamp ${this._id}`);
+//   await this.model("Course").deleteMany({ bootcamp: this._id });
+//   next();
+// });
+
+BootcampSchema.pre("remove", async function (next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`);
+  const Course = require("./Course"); // ✅ import tại đây để tránh vòng lặp
+  await Course.deleteMany({ bootcamp: this._id });
+  next();
+});
+
+// Reverse populate with virtuals
+BootcampSchema.virtual("courses", {
+  ref: "Course",
+  localField: "_id",
+  foreignField: "bootcamp",
+  justOne: false,
+});
+BootcampSchema.method("toJSON", function () {
+  console.log("toJSON called");
+});
+BootcampSchema.method("toObject", function () {
+  console.log("toObject called");
+});
+BootcampSchema.method("meow", function () {
+  console.log("meow called");
+});
+BootcampSchema.methods.getSummary = function () {
+  return `${this.name} offers careers in ${this.careers.join(", ")}`;
+};
+
+module.exports = mongoose.model("Bootcamp", BootcampSchema);

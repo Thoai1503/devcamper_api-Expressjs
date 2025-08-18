50d68941199b2f0a0e6135d0a27f79ef388688b7
Thoai1503 <vothoai1503@gmail.com>
2025-08-18 13:51:18 +0700

    db config

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
+  const conn = await mongoose.connect(
+    "mongodb+srv://thoai:thoai123@cluster0.62hjo.mongodb.net/devcamper?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  );
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
